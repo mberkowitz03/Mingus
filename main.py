ee491beab0abc5647707fd86c47b320bfdee510a
Max Berkowitz <maxberk@umich.edu>
2022-04-15 05:32:10 -0400

    note class

--- main.py
+++ main.py
@@ -31,3 +31,17 @@ class Chord:
             returnStr += "dim"
         
         return returnStr
+
+class Note:
+    note = ""
+    beatLength = 0
+    isDownbeat = False
+
+    def __init__(self, note, beatLength, isDownbeat):
+        self.note = CHROMATIC_SCALE[note % 12] #convert note number to note name
+        self.beatLength = beatLength
+        self.isDownbeat = isDownbeat
+
+    def __repr__(self):
+        return self.note
+
