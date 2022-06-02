0a003b32ea02c727908587ea48986faad10f591a
Max Berkowitz <maxberk@umich.edu>
2022-06-01 20:16:42 -0400

    added chords and polyphony support

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import music21 as m21
-global MARGIN_OF_ERROR, LENGTH_WEIGHT, CHORD_WEIGHT, DOWNBEAT_WEIGHT, DISTANCE_WEIGHT, JAZZINESS_FACTOR, CHROMATIC_SCALE, CIRCLE_OF_FIFTHS
-MARGIN_OF_ERROR = 0.005
+global CHORD_WEIGHT, DOWNBEAT_WEIGHT, DISTANCE_WEIGHT, OFF_KEY_WEIGHT, JAZZINESS_FACTOR, CHROMATIC_SCALE, CIRCLE_OF_FIFTHS
 CHORD_WEIGHT = [1.2,0.8,1]
 DOWNBEAT_WEIGHT = 0.2
 DISTANCE_WEIGHT = 0
-JAZZINESS_FACTOR = 0
+OFF_KEY_WEIGHT = 0.9 #Higher prefers out of key chords
+JAZZINESS_FACTOR = 1
 CHROMATIC_SCALE = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 CIRCLE_OF_FIFTHS = ['C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#', 'G#', 'D#', 'A#', 'F']
 
@@ -77,10 +77,9 @@ def getScale(key):
 
 
 def processMeasure(currMeasure, finalChords):
-    print("Processing measure")
     #PROCESSING HERE
     fitDict = {}
-    for chord in keyChords:
+    for chord in keyChords + nonKeyChords:
         fit = 0
         for note in currMeasure:
             if note.note in chord.notes:
@@ -95,13 +94,13 @@ def processMeasure(currMeasure, finalChords):
 
             #min is needed to find the roll-around distance. (i.e. for [1, 2, 3], 1 and 3 have a dist 1)
             dist = min(largerIndex - smallerIndex, len(CIRCLE_OF_FIFTHS) - largerIndex + smallerIndex)
-            print(dist)
             #Make algorithm strongly avoid same chords if jazziness is high.
             dist = (dist + 12 - JAZZINESS_FACTOR) % 12
             if dist == 0:
                 dist = 1
             fit -= dist * DISTANCE_WEIGHT
-
+        if chord in nonKeyChords: 
+            fit *= OFF_KEY_WEIGHT
         fitDict[chord] = fit
     return keyWithMaxFit(fitDict)
 
@@ -130,12 +129,12 @@ def processMusicXML():
     #for first note of each voice in each measure, add to currMeasure with downbeat true (check for chord)
     m21FileMeasures = m21File.parts[0].getElementsByClass(m21.stream.Measure)
     for measure in m21FileMeasures.getElementsByClass('Measure'):
-        for note in measure.notes:
-            if note.isRest:
-                continue
-            if not downBeat and note.isChord and currMeasure[-1].isDownBeat:
-                downBeat = True
-            currMeasure.append(Note(note.pitch.midi, note.quarterLength, downBeat))
+        for note in measure.flatten().notes: #Check to make sure different voices are getting a new downbeat
+            if note.isChord:
+                for chordNote in note:
+                    currMeasure.append(Note(chordNote.pitch.midi, chordNote.quarterLength, downBeat))
+            else:
+                currMeasure.append(Note(note.pitch.midi, note.quarterLength, downBeat))
             downBeat = False
         downBeat = True
         finalChords.append(processMeasure(currMeasure, finalChords))
